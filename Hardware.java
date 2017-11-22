e9fdb5a811ab0f1c06a800f66025cf156256a303
bhascup <32693665+bhascup@users.noreply.github.com>
2017-11-22 01:25:19 -0500

    Initial commit

---
+++ Hardware.java
@@ -0,0 +1,98 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This is NOT an opmode.
+ *
+ * This class can be used to define all the specific hardware for a single robot.
+ *
+ * This hardware class assumes the following device names have been configured on the robot:
+ * Note:  All names are case sensitive and some have single spaces between words.
+ */
+public class Hardware
+{
+    /* Public OpMode members. */
+
+    // Wheels
+    public DcMotor  motorFrontLeft   = null;
+    public DcMotor  motorBackLeft    = null;
+    public DcMotor  motorFrontRight  = null;
+    public DcMotor  motorBackRight   = null;
+
+    // sweeper
+    public CRServo sweepL = null;
+    public CRServo sweepR = null;
+
+
+    public DcMotor drawer = null;
+    public DcMotor clampR = null;
+    public DcMotor clampL = null;
+
+    /* Local OpMode members. */
+    HardwareMap hardwareMap = null;
+    private ElapsedTime runtime  = new ElapsedTime();
+
+    /* Constructor */
+    public Hardware() {
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // save reference to HW Map
+        hardwareMap = ahwMap;
+
+        // Wheels
+        motorFrontLeft = hardwareMap.dcMotor.get("wheel_right_front");//ok
+        motorFrontRight = hardwareMap.dcMotor.get("wheel_left_front"); //ok
+        motorBackLeft = hardwareMap.dcMotor.get("wheel_right_back");//ok
+        motorBackRight = hardwareMap.dcMotor.get("wheel_left_back");//ok
+
+
+
+        // Clamps
+
+        clampR = hardwareMap.dcMotor.get("clamp_r");//ok
+        clampL = hardwareMap.dcMotor.get("clamp_l");//ok
+
+
+        // Drawer
+
+        drawer = hardwareMap.dcMotor.get("drawer_v");// ok
+
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        motorFrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorFrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    /***
+     *
+     * waitForTick implements a periodic delay. However, this acts like a metronome with a regular
+     * periodic tick.  This is used to compensate for varying processing times for each cycle.
+     * The function looks at the elapsed cycle time, and sleeps for the remaining time interval.
+     *
+     * @param periodMs  Length of wait cycle in mSec.
+     * @throws InterruptedException
+     */
+    public void waitForTick(long periodMs)  throws InterruptedException {
+
+        long  remaining = periodMs - (long)runtime.milliseconds();
+
+        // sleep for the remaining portion of the regular cycle period.
+        if (remaining > 0)
+            Thread.sleep(remaining);
+
+        // Reset the cycle clock for the next pass.
+        runtime.reset();
+    }
+}
+
---
+++ opMode.java
@@ -0,0 +1,112 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+
+@TeleOp(name="Tele op 2017-18", group="NHRHS Robotics")
+
+public class opMode extends LinearOpMode {
+
+    Hardware robot = new Hardware();         // Uses Robot's Hardware
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        // Init Hardware Map
+        robot.init(hardwareMap);
+
+        // Initialize Robot
+        telemetry.addData("Status: ", "Its Alive!");
+        telemetry.update();
+
+
+
+
+
+//Robot is waiting to start
+        waitForStart();
+
+        while (opModeIsActive()) {
+//Show imput
+
+            //buttons
+            telemetry.addData("a: ",gamepad1.a);
+            telemetry.addData("b: ",gamepad1.b);
+            telemetry.addData("y: ",gamepad1.y);
+            telemetry.addData("x: ",gamepad1.x);
+
+           //right stick
+            telemetry.addData("right stick Y: ",(gamepad1.right_stick_y));
+            telemetry.addData("right stick X: ",(gamepad1.right_stick_x));
+
+            //left stick
+            telemetry.addData("left stick Y: ",(gamepad1.left_stick_y));
+            telemetry.addData("left stick X: ",(gamepad1.left_stick_x));
+
+            //d pad
+            telemetry.addData("d pad up:",gamepad1.dpad_up);
+            telemetry.addData("d pad down:",gamepad1.dpad_down);
+            telemetry.addData("d pad left:",gamepad1.dpad_left);
+            telemetry.addData("d pad right:",gamepad1.dpad_right);
+
+            //trigger
+            telemetry.addData("right trigger:",gamepad1.right_trigger);
+            telemetry.addData("left trigger:",gamepad1.left_trigger);
+
+            //bumper
+            telemetry.addData("right bumper:",gamepad1.right_bumper);
+            telemetry.addData("left bumper:",gamepad1.left_bumper);
+
+            //update
+            telemetry.update();
+
+//Start code for moving
+
+            // Drive train
+            robot.motorBackRight.setPower(-gamepad1.right_stick_y);
+            robot.motorFrontRight.setPower(-gamepad1.right_stick_y);
+            robot.motorBackLeft.setPower(gamepad1.left_stick_y);
+            robot.motorFrontLeft.setPower(gamepad1.left_stick_y);
+
+            //v drawer slide
+            if (gamepad1.x)
+            {
+                robot.drawer.setPower(-.25);
+            }
+
+            else if (gamepad1.y)
+            {
+                robot.drawer.setPower(1);
+            }
+
+            else
+            {
+                robot.drawer.setPower(0);
+            }
+     //clamps
+            //CHECK SIGNS
+            if (gamepad1.a){
+                robot.clampR.setPower(.25);
+                robot.clampL.setPower(-.25);
+            }
+            else if (gamepad1.b)
+            {
+                    robot.clampR.setPower(-.25);
+                    robot.clampL.setPower(.25);
+            }
+
+            else
+            {
+                robot.clampR.setPower(0);
+                robot.clampL.setPower(0);
+            }
+
+
+        }
+    }
+}
+
+
